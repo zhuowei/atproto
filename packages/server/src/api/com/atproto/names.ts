02e24bc544ade9587583471d267906db059201a1
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2022-10-23 17:35:07 -0400

    server: resolveName: resolve names from db

--- packages/server/src/api/com/atproto/names.ts
+++ packages/server/src/api/com/atproto/names.ts
@@ -3,17 +3,22 @@ import { InvalidRequestError } from '@atproto/xrpc-server'
 import * as locals from '../../../locals'
 
 export default function (server: Server) {
-  server.com.atproto.resolveName((params, _in, _req, res) => {
-    const { config, keypair } = locals.get(res)
+  server.com.atproto.resolveName(async (params, _in, _req, res) => {
+    const { db, config, keypair } = locals.get(res)
 
     let did = ''
-    if (!params.name || params.name === config.hostname) {
+    if (!params.name /* || params.name === config.hostname */) {
       // self
       did = keypair.did()
     } else if (params.name.endsWith('.test') && config.testNameRegistry) {
       did = config.testNameRegistry[params.name]
     } else {
       // @TODO
+      // zhuowei: hax
+      const user = await db.getUser(params.name)
+      if (user) {
+        did = user.did;
+      }
     }
     if (!did) {
       throw new InvalidRequestError(`Unable to resolve name`)
