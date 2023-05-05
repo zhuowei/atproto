7fe5435f476d60a1aaabdacfa21adcdcee59dcbe
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2023-05-05 01:00:57 -0400

    catch errors in force-pull

--- packages/bsky/src/api/force-pull.ts
+++ packages/bsky/src/api/force-pull.ts
@@ -6,14 +6,24 @@ export const createRouter = (ctx: AppContext): express.Router => {
 
   router.post('/forcePull/:repo/:commit', async function (req, res) {
     const {repo, commit} = req.params;
+    let err:unknown = null;
     await ctx.db.transaction(async (tx) => {
-      const indexingTx = ctx.services.indexing(tx);
-      const { root, commit: commitObj } = await indexingTx.indexRepo(repo, commit);
-      await indexingTx.indexHandle(repo, new Date().toISOString());
-      await indexingTx.setCommitLastSeen(commitObj,
-        {commit: root, rebase: false, tooBig: false}
-      );
+      try {
+        const indexingTx = ctx.services.indexing(tx);
+        const { root, commit: commitObj } = await indexingTx.indexRepo(repo, commit);
+        await indexingTx.indexHandle(repo, new Date().toISOString());
+        await indexingTx.setCommitLastSeen(commitObj,
+          {commit: root, rebase: false, tooBig: false}
+        );
+      } catch (e) {
+        console.log(e);
+        err = e;
+      }
     });
+    if (err) {
+      res.status(400).send({});
+      return;
+    }
     res.send({});
   })
 
